eb4f771ea2a749f457c92a00833ae332fb1398e0
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2026-05-19 15:58:15 +0100

    tabs

---
+++ 04-NAVIGATION/navapp/Tabs.js
@@ -0,0 +1,25 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ProfileScreen from './ProfileScreen';
+import HomeScreen from './HomeScreen';
+
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+      />
+
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+      />
+    </Tab.Navigator>
+  );
+}
+
+export default tabs
