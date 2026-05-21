d6be093d435cd37779c41013bb31c622de2d3210
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2026-05-21 14:24:25 +0100

    added drawer nav

---
+++ 04-NAVIGATION/navapp/DrawerNav.js
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Tabs from './Tabs';
+
+const Drawer = createDrawerNavigator();
+
+const DrawerNav = () => {
+  return (
+    <Drawer.Navigator>
+          <Drawer.Screen name="Dashboard" component={Tabs}>
+              
+          </Drawer.Screen>
+    </Drawer.Navigator>
+  )
+}
+
+export default DrawerNav
