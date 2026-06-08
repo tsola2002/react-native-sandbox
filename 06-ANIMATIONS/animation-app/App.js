80f3ae293811d70a3e0946f9fddcd874435b863a
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2026-06-08 15:23:29 +0100

    slide

--- 06-ANIMATIONS/animation-app/App.js
+++ 06-ANIMATIONS/animation-app/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import FadeInBox from './FadeInBox';
+import SlideBox from './SlideBox';
 
 export default function App() {
   return (
-    <FadeInBox/>
+    // <FadeInBox/>
+    <SlideBox />
   );
 }
 
