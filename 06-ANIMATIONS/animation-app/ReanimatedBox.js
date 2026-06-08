201439aace6157545f699ff2d6143ae518f815f2
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2026-06-08 16:09:44 +0100

    added reanimated

---
+++ 06-ANIMATIONS/animation-app/ReanimatedBox.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ReanimatedBox = () => {
+  return (
+    <View>
+      <Text>ReanimatedBox</Text>
+    </View>
+  )
+}
+
+export default ReanimatedBox
